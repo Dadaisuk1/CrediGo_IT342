8d8e4c43190681299a688900095479608f3e583f
Jake Luis Lood <jakeluis.lood@cit.edu>
2025-04-30 13:29:31 +0800

    Connect front and backend

--- react-frontweb/src/services/api.jsx
+++ react-frontweb/src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'https://credigo-it342.onrender.com/api';
 
 // Create an Axios instance with default settings
 const apiClient = axios.create({
