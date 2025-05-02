2e390428112397c099b2d34ca41e033b5dca2135
MutsarelaCheese <Darwindarryljean.largoza@gmail.com>
2025-05-03 02:41:27 +0800

    Hardcode

--- spring-backend/src/main/java/com/credigo/backend/CredigoBackendApplication.java
+++ spring-backend/src/main/java/com/credigo/backend/CredigoBackendApplication.java
@@ -1,14 +1,12 @@
 package com.credigo.backend;
 
-import com.credigo.backend.config.DotenvLoader;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CredigoBackendApplication {
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(CredigoBackendApplication.class);
-        app.addInitializers(new DotenvLoader());
-        app.run(args);
+        SpringApplication.run(CredigoBackendApplication.class, args);
     }
 }
